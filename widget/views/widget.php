ef07f4dfc3e69cbc6ffe7f512a1cd8bb24728833
James Doc <hi@jamesdoc.com>
2015-10-14 11:26:29 +0100

    Display dates in the widget

--- widget/views/widget.php
+++ widget/views/widget.php
@@ -1,10 +1,9 @@
 <!-- This file is used to markup the public-facing widget. -->
-
-
 <?php if($events->have_posts()):?>
-
+	
 	<?php while ( $events->have_posts() ) : $events->the_post(); ?>
 		<p class="sse-widget-entry-title">
+			<small><?php echo get_post_meta(get_the_ID(), 'sse_start_date', true); ?></small>
 			<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark"><?php the_title(); ?></a>
 		</p>
 	<?php endwhile; ?>
